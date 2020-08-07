e3a888efe53e6be34afb729f2f729bbe8c208f6b
Michael Clapham <michaeljclapham@gmail.com>
2020-08-07 22:48:21 +0100

    Add card via Github web editor

--- cards.js
+++ cards.js
@@ -8,5 +8,10 @@ var cards = [
         "rank": 7,
         "suit": "clubs",
         "text": "Another card..."
+    },
+    {
+        "rank": "j",
+        "suit": "diamonds",
+        "text": "I added this card via Github's web file editor!"
     }
 ]
