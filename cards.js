c5f7536d906e85916eefaa5a9061c56f295011d3
smborio <smborio@idems.international>
2020-08-07 18:54:27 -0300

    Created a third card to test things

--- cards.js
+++ cards.js
@@ -8,5 +8,10 @@ var cards = [
         "rank": 7,
         "suit": "clubs",
         "text": "Another card..."
+    },
+    {
+        "rank": "K",
+        "suit": "spades",
+        "text": "this would be the king of spades"
     }
 ]
