49775a5b827e3a4976cb335c3928ff2de798e1a3
Mike Nsubuga <nsubugamike021@gmail.com>
2020-11-05 16:48:44 +0300

    added list of cards on home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+import { CardComponent } from "./components/card/card.component";
+import { HomeComponent } from "./components/home/home.component";
+
+const routes: Routes = [
+  { path: "cards", component: CardComponent },
+  { path: "", component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
