68beb107e44a2528e00d4d6365ea1a71548bdffd
Mike Nsubuga <nsubugamike021@gmail.com>
2020-11-14 22:59:20 +0300

    added export feature to cards

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { CardService } from "src/app/card.service";
 import { ActivatedRoute } from "@angular/router";
+import jspdf from "jspdf";
+import html2canvas from "html2canvas";
+import { ViewChild } from "@angular/core";
+import { ElementRef } from "@angular/core";
 
 @Component({
   selector: "app-card",
   templateUrl: "./card.component.html",
   styleUrls: ["./card.component.less"],
-  encapsulation: ViewEncapsulation.ShadowDom,
+  encapsulation: ViewEncapsulation.None,
 })
 export class CardComponent implements OnInit {
   content;
@@ -20,7 +24,6 @@ export class CardComponent implements OnInit {
   }
 
   ngOnInit(): void {}
-
   getCardContent() {
     this.cardService
       .readCardContent(this.route.snapshot.params.slug)
@@ -47,6 +50,23 @@ export class CardComponent implements OnInit {
         (t) => t.slug === this.route.snapshot.params.slug
       );
       this.card = ncard;
+    });
+  }
+
+  exportCard() {
+    const data = document.getElementById("cardPDF");
+    console.log("id ", data);
+    html2canvas(data).then((canvas) => {
+      const contentDataURL = canvas.toDataURL("image/png");
+      // let pdf = new jspdf("p", "mm", "a4");
+      // pdf.addImage(contentDataURL, "png", 0, 0, 210, 0); a4
+      // pdf.addImage(contentDataURL, "png", 0, 0, 63, 0);  -- deck szie
+
+      //pdf.addImage(contentDataURL, "png", 0, 0, 105, 0); - a6
+
+      let pdf = new jspdf("p", "mm", [63.5, 88.8]);
+      pdf.addImage(contentDataURL, "png", 0, 0, 63, 0);
+      pdf.save(this.card.title);
     });
   }
 }
