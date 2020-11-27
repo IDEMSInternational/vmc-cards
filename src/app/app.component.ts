f8d1acfe2450fa3df080f18cf0690fb01e33659d
Mike Nsubuga <nsubugamike021@gmail.com>
2020-11-27 09:22:58 +0300

    disabled print features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,6 @@ import { ChangeDetectorRef, Component, ViewEncapsulation } from "@angular/core";
 })
 export class AppComponent {
   title = "vmc-cards";
-  printStyle = "Web";
-  isPrinting = false;
 
   constructor(private cdr: ChangeDetectorRef) {}
-
-  export() {
-    window.onbeforeprint = () => {
-      this.togglePrintMode();
-    };
-    window.onafterprint = () => {
-      this.togglePrintMode();
-    };
-    // use timeout to allow style updates after isPrinting is enabled
-    setTimeout(() => {
-      window.print();
-    }, 50);
-  }
-  togglePrintMode() {
-    this.isPrinting = !this.isPrinting;
-    this.cdr.detectChanges();
-  }
 }
