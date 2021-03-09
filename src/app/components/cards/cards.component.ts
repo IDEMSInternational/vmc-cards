baa7ce7fa8092ab35aa3517720a6ae32535093d4
Michael Clapham <michaeljclapham@gmail.com>
2021-03-09 12:15:02 +0000

    Put /cards in front of assets paths

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -30,7 +30,7 @@ export class CardsComponent {
   }
 
   replaceImageURLS(content) {
-    const updatedContent = content.replace(/images/g, "assets/images");
+    const updatedContent = content.replace(/images/g, "/cards/assets/images");
     return updatedContent;
   }
 }
