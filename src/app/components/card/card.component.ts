5fa9b0f8bd5206f0976d1fbb0c8a6cb6c425f635
Mike Nsubuga <nsubugamike021@gmail.com>
2021-03-23 14:58:18 +0300

    display images for card explanations

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -24,13 +24,11 @@ export class CardComponent implements OnInit {
     this.cardService
       .readCardContent(this.route.snapshot.params.slug)
       .subscribe((data) => {
-        this.content = data;
-        this.replaceImageURLs(this.content);
-        console.log("card content", this.content);
+        this.content = this.replaceImageURLs(data);
       });
   }
 
-  async replaceImageURLs(cardContent) {
+  replaceImageURLs(cardContent) {
     const originalContent = JSON.stringify(cardContent);
     const updatedContent = originalContent.replace(/\images/g, "assets/images");
     const newContent = JSON.parse(updatedContent);
