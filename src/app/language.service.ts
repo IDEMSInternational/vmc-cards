61779cc44803c8d5c715c2bc1426146e27b34030
Mike Nsubuga <nsubugamike021@gmail.com>
2021-08-11 09:52:32 +0300

    created lang service

---
+++ src/app/language.service.ts
@@ -0,0 +1,10 @@
+import { Inject, Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
+import { ActivatedRoute, Router } from "@angular/router";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LanguageService {
+  constructor() {}
+}
