0d422308e14a67fc0618ee02ea45075f25ea90c6
Fidel365 <107605960+Fidel365@users.noreply.github.com>
2024-08-01 12:55:54 +0300

    French translations

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { CardService } from "src/app/services/card.service";
 import { AppService } from "src/app/services/app.service";
+import { LanguageService } from "src/app/services/language.service";
 
 @Component({
   selector: "app-cards",
@@ -9,6 +10,14 @@ import { AppService } from "src/app/services/app.service";
   encapsulation: ViewEncapsulation.None,
 })
 export class CardsComponent implements OnInit {
-  constructor(public cardService: CardService, public appService: AppService) {}
-  ngOnInit() {}
+  languageCode: string;
+  constructor(public cardService: CardService, public appService: AppService, public languageService: LanguageService) { }
+  ngOnInit() {
+    this.subscribeToLanguageChanges()
+  }
+  subscribeToLanguageChanges() {
+    this.languageService.activeLanguage$.subscribe((languageCode) => {
+      this.languageCode = languageCode
+    })
+  }
 }
