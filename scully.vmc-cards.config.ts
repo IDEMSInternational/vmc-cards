48bb2e2eeaf531a0b217301a2e6a3d04020c9bad
chrismclarke <chris.m.clarke@live.co.uk>
2025-07-05 10:54:03 -0700

    fix: pupetter chromium browser

--- scully.vmc-cards.config.ts
+++ scully.vmc-cards.config.ts
@@ -1,29 +1,23 @@
-let cardRoutes = [];
-for (let suit of ["C", "S", "H", "D"]) {
+const cardRoutes: string[] = [];
+for (const suit of ["C", "S", "H", "D"]) {
   for (let i = 1; i < 14; i++) {
     let value = "" + i;
-    if (i === 1) {
-      value = "A";
-    }
-    if (i === 11) {
-      value = "J";
-    }
-    if (i === 12) {
-      value = "Q";
-    }
-    if (i === 13) {
-      value = "K";
-    }
-    cardRoutes.push("/" + value + suit);
+    if (i === 1) value = "A";
+    if (i === 11) value = "J";
+    if (i === 12) value = "Q";
+    if (i === 13) value = "K";
+    cardRoutes.push(`/${value}${suit}`);
   }
 }
 
-import { ScullyConfig } from '@scullyio/scully';
+import { ScullyConfig } from "@scullyio/scully";
 export const config: ScullyConfig = {
   projectRoot: "./src",
   projectName: "vmc-cards",
-  outDir: './dist/static',
-  routes: {
+  outDir: "./dist/static",
+  routes: {},
+  extraRoutes: cardRoutes,
+  puppeteerLaunchOptions: {
+    executablePath: process.env.CHROMIUM_PATH || undefined,
   },
-  extraRoutes: cardRoutes
 };
